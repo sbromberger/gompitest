ba7b70df7b05245e7b15fa870015f42fb8e59bf0
Seth Bromberger <github@bromberger.com>
2021-12-01 14:10:34 -0500

    pingpongraw

---
+++ pingpongraw/pingpongraw.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	mpi "github.com/sbromberger/gompi"
+)
+
+func main() {
+	str := "Hello this is a message"
+
+	mpi.Start(true)
+	fmt.Println("started")
+	o := mpi.NewCommunicator(nil)
+	myRank := o.Rank()
+
+	if myRank == 0 {
+		fmt.Println("rank 0")
+		t0 := time.Now()
+		o.SendString(str, 1, 0)
+		t1 := time.Now()
+		_, _ = o.RecvString(1, 0)
+		t2 := time.Now()
+
+		fmt.Printf("sent in %v, round trip %v\n", t1.Sub(t0), t2.Sub(t0))
+	} else {
+		s, _ := o.RecvString(0, 0)
+		o.SendString(s, 0, 0)
+	}
+
+	mpi.Stop()
+}
