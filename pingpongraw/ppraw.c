3f8f0b15a1853f7d06143a42ecff1e871a3d9b25
Seth Bromberger <github@bromberger.com>
2021-12-01 15:32:55 -0500

    ppraw.c

---
+++ pingpongraw/ppraw.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <mpi.h>
+#include <time.h>
+int main(int argc, char** argv){
+    int node;
+    int t;
+    MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &t);
+    MPI_Comm_rank(MPI_COMM_WORLD, &node);
+    char *data = "hello there";
+    int l = 11;
+    clock_t start, finish;
+
+    MPI_Message msg;
+    MPI_Status status;
+
+
+    if (node == 0) {
+	    printf("node 0 warming up\n");
+	    fflush(stdout);
+	    printf("node 0 warmup sending\n");
+	    MPI_Send(data, l, MPI_BYTE, 1, 0, MPI_COMM_WORLD);
+	    printf("node 0 warmup mprobing\n");
+	    MPI_Mprobe(1, 0, MPI_COMM_WORLD, &msg, &status);
+	    int count;
+	    MPI_Get_count(&status, MPI_BYTE, &count);
+	    char* buffer = malloc(count);
+
+	    printf("node 0 warmup recving\n");
+            MPI_Mrecv(buffer, count, MPI_BYTE, &msg, MPI_STATUS_IGNORE);
+	    printf("node 0 sending\n");
+	    fflush(stdout);
+	    start = clock();
+	    MPI_Send(data, l, MPI_BYTE, 1, 0, MPI_COMM_WORLD);
+	    MPI_Mprobe(1, 0, MPI_COMM_WORLD, &msg, &status);
+	    MPI_Get_count(&status, MPI_BYTE, &count);
+	    char *buffer2 = malloc(count);
+
+            MPI_Mrecv(buffer2, count, MPI_BYTE, &msg, MPI_STATUS_IGNORE);
+	    finish = clock();
+	    double cpu_time_used;
+	    cpu_time_used = ((double) (finish - start)) / CLOCKS_PER_SEC * 1000000;
+	    printf("Elapsed: %f us\n", cpu_time_used);
+	    fflush(stdout);
+    } else {
+	    printf("node 1 warmup mprobing\n");
+	    fflush(stdout);
+	    MPI_Mprobe(0, 0, MPI_COMM_WORLD, &msg, &status);
+	    printf("node 1 warmup recving\n");
+	    int count;
+	    MPI_Get_count(&status, MPI_BYTE, &count);
+	    char* buffer = malloc(count);
+
+	    MPI_Mrecv(buffer, l, MPI_BYTE, &msg, &status);
+	    printf("node 1 warmup sending\n");
+	    MPI_Send(data, l, MPI_BYTE, 0, 0, MPI_COMM_WORLD);
+
+	    
+	    MPI_Mprobe(0, 0, MPI_COMM_WORLD, &msg, &status);
+	    MPI_Get_count(&status, MPI_BYTE, &count);
+	    char* buffer2 = malloc(count);
+
+	    MPI_Mrecv(buffer2, l, MPI_BYTE, &msg, &status);
+	    MPI_Send(data, l, MPI_BYTE, 0, 0, MPI_COMM_WORLD);
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Finalize();
+}
