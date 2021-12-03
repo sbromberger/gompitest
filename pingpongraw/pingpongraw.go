4625330f9592fec5d689907dd0093c546de89dcd
Seth Bromberger <github@bromberger.com>
2021-12-03 10:15:23 -0500

    bench

--- pingpongraw/pingpongraw.go
+++ pingpongraw/pingpongraw.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 
 	mpi "github.com/sbromberger/gompi"
 )
 
 func main() {
+	n, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		panic("Invalid number of iterations")
+	}
+
 	str := "Hello this is a message"
 
 	mpi.Start(true)
@@ -14,23 +21,23 @@ func main() {
 	o := mpi.NewCommunicator(nil)
 	myRank := o.Rank()
 
-	if myRank == 0 {
-		o.SendString(str, 1, 0)
-		_, _ = o.RecvString(1, 0)
-		fmt.Println("rank 0")
-		t0 := mpi.WorldTime()
-		o.SendString(str, 1, 0)
-		t1 := mpi.WorldTime()
-		_, _ = o.RecvString(1, 0)
-		t2 := mpi.WorldTime()
+	t0 := mpi.WorldTime()
+	for i := 0; i < n; i++ {
+		if myRank == 0 {
+			o.SendString(str, 1, 0)
+			_, _ = o.RecvString(1, 0)
+			o.SendString(str, 1, 0)
+			_, _ = o.RecvString(1, 0)
 
-		fmt.Printf("sent in %v µs, round trip %v µs\n", (t1-t0) * 100000, (t2-t0) * 100000)
-	} else {
-		s, _ := o.RecvString(0, 0)
-		o.SendString(s, 0, 0)
-		_, _ = o.RecvString(0, 0)
-		o.SendString(s, 0, 0)
+		} else {
+			s, _ := o.RecvString(0, 0)
+			o.SendString(s, 0, 0)
+			_, _ = o.RecvString(0, 0)
+			o.SendString(s, 0, 0)
+		}
 	}
+	t1 := mpi.WorldTime()
 
+	fmt.Printf("elapsed %v s, average %v µs\n", t1-t0, (t1-t0)/float64(n)*100000)
 	mpi.Stop()
 }
